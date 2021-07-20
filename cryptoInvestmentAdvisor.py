02eb57b167de5ead5b09c60d11588cecd984a151
Atrone <ateixei4@uwo.ca>
2021-07-19 22:08:45 -0400

    fix some buys bugs

--- cryptoInvestmentAdvisor.py
+++ cryptoInvestmentAdvisor.py
@@ -26,12 +26,12 @@ def getBuysForCryptosAccordingToTrends(result,totalOverTrend,dollarAmount):
     return buys
 
 
-def getBuysForCryptos(result,dollarAmount,results):
+def getBuysForCryptos(result,dollarAmount):
     buys = []
     for key, value in result['price'].items():
         for key1, value1 in result['trends'].items():
             if key == key1:
-                buys.append({key1:(dollarAmount/len(results[0].keys()))/value})
+                buys.append({key1:(dollarAmount/len(result['price'].keys()))/value})
                 break
     print(buys)
     return buys
@@ -48,6 +48,7 @@ def getCurrentPortfolioWorthFromBuys(buys):
     # total / trend proportionate to price / crypto
     # it's a hype market
 
+results = [{'price': {'BTC crypto': 31796.810138247813, 'ETH crypto': 1895.5521368013024, 'USDT crypto': 1.00062070229279, 'BNB crypto': 302.4804892006454, 'ADA crypto': 1.18369795434564, 'USDC crypto': 1.00057362658003, 'XRP crypto': 0.58792189594955, 'DOGE crypto': 0.18223326670003, 'DOT crypto': 12.68214591960496, 'BUSD crypto': 1.00048936213312}, 'trends': {'BTC crypto': 60157.0, 'ETH crypto': 60325, 'USDT crypto': 53684, 'BNB crypto': 56661.0, 'ADA crypto': 58618.0, 'USDC crypto': 37877, 'XRP crypto': 57397.0, 'DOGE crypto': 56497.0, 'DOT crypto': 41280, 'BUSD crypto': 39459}, 'date': datetime.datetime(2021, 7, 19, 0, 0)}]
 
 def getResults(results : List[Dict], amountToInvest):
     for result in results:
@@ -64,7 +65,7 @@ def getResults(results : List[Dict], amountToInvest):
 
         print(getCurrentPortfolioWorthFromBuys(buysAccordingToTrends))
 
-        buys = getBuysForCryptos(result, amountToInvest,results)
+        buys = getBuysForCryptos(result, amountToInvest)
 
         tools.updateCSVForListOfDict(tools.mergeListOfDicts(buys), "nonTrendBuys"+
                                 str(datetime.datetime.now().month)+
@@ -73,3 +74,6 @@ def getResults(results : List[Dict], amountToInvest):
         print(getCurrentPortfolioWorthFromBuys(buys))
 
         time.sleep(65)
+
+if __name__ == "__main__":
+    getResults(results,100)
--- main.py
+++ main.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     assert int(os.environ['amountToInvest'])
     if(os.environ['generate'] == "Y"):
         cryptoInvestmentAdvisor.getResults(historicalCryptoTrendsGenerator.getResults(
-            datetime.datetime(2021, 7, 11), 336, 10)
+            datetime.datetime(2021, 7, 19), 336, 10)
                                            ,int(os.environ['amountToInvest']))
     elif(os.environ['generate'] == "N"):
-        cryptoInvestmentAdvisor.getResults(tools.generateHistoricalCryptoTrendsGeneratorResultsFromCSV(datetime.datetime(2021, 7, 11))
+        cryptoInvestmentAdvisor.getResults(tools.generateHistoricalCryptoTrendsGeneratorResultsFromCSV(datetime.datetime(2021, 6, 20))
                                            ,int(os.environ['amountToInvest']))
     else:
         print("please enter a valid generate value")
